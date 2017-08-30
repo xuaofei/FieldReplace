8eb3f6eec46964936927f954a4fd80d86687657c
lhy <lhy@LHY-PC>
2017-08-30 20:42:21 +0800

    添加项目文件。

---
+++ FieldReplace/FieldReplace.h
@@ -0,0 +1,32 @@
+
+// FieldReplace.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CFieldReplaceApp: 
+// 有关此类的实现，请参阅 FieldReplace.cpp
+//
+
+class CFieldReplaceApp : public CWinApp
+{
+public:
+	CFieldReplaceApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CFieldReplaceApp theApp;
